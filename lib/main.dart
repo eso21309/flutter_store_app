ca6ced3281a82f97a82ea58039c85b36af45e477
GitHub eso21309 <GitHub eso21309@gmail.com>
2024-11-13 15:19:14 +0900

    1차 결과물

---
+++ lib/main.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: StorePage(),
+    );
+  }
+}
+
+class StorePage extends StatelessWidget {
+  const StorePage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 64,
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                child: Row(
+                  children: [
+                    Text(
+                      'Woman',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Spacer(),
+                    Text(
+                      'Kids',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Spacer(),
+                    Text(
+                      'Shoes',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Spacer(),
+                    Text(
+                      'Bag',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Expanded(
+                child: Image.asset(
+              'assets/bag.jpg',
+              fit: BoxFit.cover,
+            )),
+            Expanded(
+                child: Image.asset(
+              'assets/shoes.jpg',
+              fit: BoxFit.cover,
+            )),
+          ],
+        ),
+      ),
+    );
+  }
+}
